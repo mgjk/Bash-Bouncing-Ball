1d636c43e9a57d1500be022ba3df2c3ed4596a63
Mike Kallies <mike.kallies@gmail.com>
2013-07-18 09:27:40 -0400

    Added bouncy.bash

---
+++ bouncy.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+x=1
+y=1
+
+dx=1
+dy=1
+
+maxx=80
+maxy=25
+
+while [ 1 ] 
+do
+	# remove previous character
+	echo -en "\033[$y;${x}H "
+
+	x=$((x+dx))
+	y=$((y+dy))
+
+	# place new character
+	echo -en "\033[$y;${x}Ho"
+	
+	if [ $x -ge $maxx -o $x -lt 1 ]
+	then
+		dx=$((-1*dx))
+	fi
+
+	if [ $y -ge $maxy -o $y -lt 1 ]
+	then
+		dy=$((-1*dy))
+	fi
+
+	i=0
+	while [ $i -lt 100 ]
+	do
+		i=$((i+1))
+	done
+#	sleep 1
+
+done
+
